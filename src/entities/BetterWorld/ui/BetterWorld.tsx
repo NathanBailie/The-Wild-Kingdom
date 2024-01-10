696e49b77163eab430e905214ccf477cf203c013
Nathan <antnefedov33@gmail.com>
2024-01-10 21:03:28 +0300

    updated headers main image

--- src/entities/BetterWorld/ui/BetterWorld.tsx
+++ src/entities/BetterWorld/ui/BetterWorld.tsx
@@ -1,15 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
-import cls from './BetterWorld.module.scss';
 import mainDog from 'shared/assets/img/main-dog.png';
+import dogHead from 'shared/assets/img/main-dog-head.png';
 import cat1 from 'shared/assets/icons/betterWorld/cat1.png';
 import dog2 from 'shared/assets/icons/betterWorld/dog2.png';
 import cat3 from 'shared/assets/icons/betterWorld/cat3.png';
+import cls from './BetterWorld.module.scss';
 
 export const BetterWorld = memo(() => {
     return (
         <section className={classNames(cls.BetterWorld, {}, [])}>
-            <img src={mainDog} alt="dog" className={cls.BetterWorld__mainImg} />
+            <div className={cls.BetterWorld__mainImgWrapper}>
+                <div className={cls.BetterWorld__mainImg}>
+                    <img src={mainDog} alt="dog" />
+                </div>
+                <img src={dogHead} alt="dogs-head" className={cls.BetterWorld__mainImgHead} />
+            </div>
             <div className={classNames(cls.BetterWorld__container, {}, ['container'])}>
 
                 <h1>
